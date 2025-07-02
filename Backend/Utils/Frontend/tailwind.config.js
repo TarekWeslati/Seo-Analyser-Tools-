ebebe9be2af43fbb2fa16b9ffb8babcb9dde82ed
TarekWeslati <oueslatit1@gmail.com>
2025-07-02 21:17:49 +0100

    Create tailwind.config.js

---
+++ Backend/Utils/Frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Enable dark mode based on 'dark' class on HTML
+  content: [
+    "../public/**/*.html",
+    "../src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
