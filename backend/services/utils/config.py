add656e2198c74b7fe5756d37ebb54b7372e62c2
TarekWeslati <oueslatit1@gmail.com>
2025-07-05 23:25:37 +0100

    Delete backend/services/utils directory

--- backend/services/utils/config.py
+++
@@ -1,6 +0,0 @@
-# This file is illustrative. Prefer .env for API keys.
-# config.py
-# class Config:
-#     PAGESPEED_API_KEY = os.getenv('PAGESPEED_API_KEY')
-#     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-#     # ... other configurations
