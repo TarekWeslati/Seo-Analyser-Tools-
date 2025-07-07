3746d89bd756c50a2a2a131baedf7d6e3a86060a
TarekWeslati <oueslatit1@gmail.com>
2025-07-07 22:31:42 +0100

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+  plugins: {
+    tailwindcss: {}, // هذا السطر يخبر PostCSS باستخدام Tailwind CSS
+    autoprefixer: {}, // هذا السطر يخبر PostCSS باستخدام Autoprefixer
+  },
+};
