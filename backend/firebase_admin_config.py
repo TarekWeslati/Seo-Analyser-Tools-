f0779adb49dc68a52651a99c631d69b49e3b29cc
TarekWeslati <oueslatit1@gmail.com>
2025-07-31 23:06:21 +0100

    Update firebase_admin_config.py

--- backend/firebase_admin_config.py
+++ backend/firebase_admin_config.py
@@ -1,13 +1,6 @@
-{
-  "type": "service_account",
-  "project_id": "message-oxabite",
-  "private_key_id": "c4eaf002aeac038036f0152161c8253def2b7ba4",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCUh2fprl8N9oQK\n5iqbmYLIZ5DCbqQGLl4onCdVgsckrd/82f9f8P9ig7kGgybd5ffIYGgxmIfWU9lN\ni6sbWu0CGQy4EIa7tTI05xuRfMAo9PgElTnRM/hNV7/+nBg3nEoU/kAEXcV2p9HX\naXHIFfdVk70XdEof//i8Vi8JFuBsE1DipxeXjI2TQJBKfu2ZpQpONnwodaH2QKqr\n2Pa6V8brZ8RVHv8CFk0PVi7LexX5vXdAjli1HWfgeRAinWjQ93aOYtIGV0yykK4q\nNraN+mRpGBx2wA2aoL9uPhPYjVtIukaDlkB752d4GVw1O1oyuv+MveTnX5mxSG7u\nqvo1N/EPAgMBAAECggEAPLsk/QLEcSQkF4THVToupJzcLvQoczKQY5B98NP5c4D4\n/h1w8xzKf9CWXhhQUxkN+MuarvTMThGY38FFJHlIQbxWXAi93pCXR9RbjGBy/gw0\nQUN3EZhrZcyEf6n5qPtpuy2+pUpmTD8QaUD/Oi3pJP+0qJtvF6iFXxTqRJArEWUw\nxmHfHhzzpxurnfop96FZsSvuA2bMSAIFdcaxgbJCNWVtsq+YohUDemrtnIqVvS8V\nHN/I5ktimqyo2+6fwwhBc8xlY5a2R4TGQgWiY+XEc0Jki4GD0RWoZBLdPGpoVcAL\nbNexom9gwHnMaJzKsRLfSFmD8AuKNIZ97zMzanNbGQKBgQDO4XBoIap3pZ4Pq+Nr\nzM1P7xwqu7MfxBkUagjJWe6lz1UyCKXm15o5KPeIfv7zcF7fABvMXzFvrfcz4yHN\n2to+THxIVoGNI4XDV5r+Mc1E8ijodJTKsxDXWlmjwQVF2Puq9pVYmbIzsY7AGDGJ\nb1w6wgF55dSDNZEPjL7veR8giwKBgQC3y0AXMzNM8fgJKxm50p6RRxlVEi/KufsN\n89GKaZzchSwFRY25+6N9RMzgGfIutbBjJ6XmPbYA/FtJ3dV3du5Hm+w9R5aaJ8VX\nuQj8qUA3dlWC63XP8aB1OIlQwSAUSeGUoqGPKStv3gSp+xzVTWGmgX4jIZIsio4z\n9zXV2dkeDQKBgQCxzeQUO0sNhhQxF+RgFCcpWjJ1L9klph6AAxoGI/sOGwH3/KLJ\noef01Jbag3uPsrZ3Jtq9bdtJRtU9jSrSiBQmkwoGCgNSUIN4fmTBr//iD9bE4t22\n0Ovaitab/RsRCwpRx5hYC9CyyRoNUz3QBxvXjEJEiBl7o6BHIwo/O3RbPQKBgQCg\nPJp+34udTOQTkjkWkfLe8965FWcHVsfzbRvFkbhJZe1wFyit+h6aS05XHyfO42aJ\nu/peVnUEPP+V0cqXIeAyHFWFQ5GgioX5qf+U0DaFZCy/PGt1RNLA/+5vskA2kAcU\noAk8mAcfu6/TRnPn31QAZ6tCE7WuEYpS8fdEn240TQKBgDyfj6eor+fXROtC/H9O\nFY6Va0RSkcOwwd7mWf9cDC3OFU1LpJGzSX0HiPl92DaLmPNXETjkUfdtT4qTOKBB\n9WyNEXm84xiNPMICGT2zICeaD37Dw8gOwBYI7DSGngAxDTOkOBdUrSndAGOun5bV\ngyIDcr0+Ha+2P23KEOCfmsnv\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@message-oxabite.iam.gserviceaccount.com",
-  "client_id": "109939010200979159303",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40message-oxabite.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
+        # هذا الملف كان يحتوي على مفتاح حساب الخدمة الخاص بك من Firebase.
+        # تم حذف المحتوى من هنا وسيتم تحميله كمتغير بيئة في Render.
+
+        # يجب أن يكون هذا الملف فارغاً من أي معلومات حساسة عند رفعه إلى GitHub.
+        FIREBASE_SERVICE_ACCOUNT_KEY = None # أو يمكنك إزالته تماماً إذا لم تعد تستخدمه هنا
+        
