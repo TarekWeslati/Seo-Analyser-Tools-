a375f97c415878429805848c49f57b135cf7ad5a
TarekWeslati <oueslatit1@gmail.com>
2025-08-05 12:21:16 +0100

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -4,4 +4,4 @@ import os
 # Add the backend directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from backend.app import app as application # Renamed app to application as per WSGI convention
+from backend.app import app # Import Flask app named 'app'
