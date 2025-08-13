6fa3dec53bd0f4399dd4a5a34daeae3bdc1d7029
TarekWeslati <oueslatit1@gmail.com>
2025-08-13 23:14:17 +0100

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,6 @@
-# Import the Flask application object from your app.py file
-from app import app
+# wsgi.py
+# This file serves as the entry point for Gunicorn.
+from backend.app import app
 
-# This file is used by the Gunicorn server to run the application.
-# The 'app' variable is the entry point.
 if __name__ == "__main__":
     app.run()
