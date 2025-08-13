d0d63e80b0e3abdcc58e0f55aacb21db52fe61c6
TarekWeslati <oueslatit1@gmail.com>
2025-08-13 23:24:46 +0100

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,8 @@
 # wsgi.py
 # This file serves as the entry point for Gunicorn.
-from backend.app import app
+# It imports the 'app' object from the 'app.py' file.
+
+from app import app as application
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
