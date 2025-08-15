c4e0d3bf0759e5b516b02bd70a00ceceb42f502b
TarekWeslati <oueslatit1@gmail.com>
2025-08-15 21:13:24 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -196,4 +196,4 @@ def analyze_competitors():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=true)
